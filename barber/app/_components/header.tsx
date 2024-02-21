27ae8f428883536a4f59238a412cd022bd8e6cff
XavierRLX <91702228+XavierRLX@users.noreply.github.com>
2024-02-21 20:57:35 -0300

    feat
    add initial header compoents

---
+++ barber/app/_components/header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+    return (
+        <Card >
+            <CardContent className="py-5 px-30 justify-between items-center flex flex-row">
+                <Image src="/logo.png" alt="FSWBarber" height={22} width={120}/>
+                <Button variant={"outline"} size={"icon"} className="h-8 w-8">
+                    <MenuIcon/>
+                </Button>
+            </CardContent>
+        </Card>
+    );
+}
+ 
+export default Header ;
