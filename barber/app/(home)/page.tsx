f04fac36f9cbe80de1a33f5ede9a37aa089b04dc
XavierRLX <91702228+XavierRLX@users.noreply.github.com>
2024-02-26 20:18:08 -0300

    feat
    add initial search components

--- barber/app/(home)/page.tsx
+++ barber/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Header from "../_components/header"
 import { format } from "date-fns"
 import { ptBR } from "date-fns/locale"
+import Seach from "./_components/search"
 
 export default function Home() {
   return <div>
@@ -14,5 +15,9 @@ export default function Home() {
         })}
       </p>
     </div>
+
+    <div className="px-5 mt-6">
+      <Seach/>
+      </div>
   </div>
 }
