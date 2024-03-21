8a1f21c9e2b353ff28d366f6d9014fe176f2a355
XavierRLX <renanlima2000.aer@gmail.com>
2024-03-21 09:26:24 -0300

    feat ww
    tentativa de colocar auth o pra

--- barber/app/_components/header.tsx
+++ barber/app/_components/header.tsx
@@ -1,16 +1,23 @@
+"use client"
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import { singIn } from "next-auth/react"
 
 const Header = () => {
+    const handleLoginClick = async () => {
+        await singIn();
+    };
     return (
         <Card >
             <CardContent className="py-5 px-30 justify-between items-center flex flex-row">
                 <Image src="/logo.png" alt="FSWBarber" height={22} width={120}/>
-                <Button variant={"outline"} size={"icon"} className="h-8 w-8">
+                {/* <Button variant={"outline"} size={"icon"} className="h-8 w-8">
                     <MenuIcon/>
-                </Button>
+                </Button> */}
+                <Button onClick={handleLoginClick}>Login</Button>
             </CardContent>
         </Card>
     );
