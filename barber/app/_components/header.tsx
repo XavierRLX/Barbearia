cfd0c81d5854a20e44baa30eb85f01d784f010f8
XavierRLX <91702228+XavierRLX@users.noreply.github.com>
2024-03-22 14:18:39 -0300

    feat
    add
    Sheeet (menu lateral)

--- barber/app/_components/header.tsx
+++ barber/app/_components/header.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { signIn, signOut, useSession } from "next-auth/react"
 import { MenuIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
 
 
 const Header = () => {
@@ -12,9 +13,18 @@ const Header = () => {
         <Card >
             <CardContent className="py-5 px-30 justify-between items-center flex flex-row">
                 <Image src="/logo.png" alt="FSWBarber" height={22} width={120}/>
-                <Button variant={"outline"} size={"icon"} className="h-8 w-8">
-                    <MenuIcon size={16}/>
-                </Button>
+                <Sheet>
+                    <SheetTrigger>
+                        <Button variant={"outline"} size={"icon"} className="h-8 w-8">
+                            <MenuIcon size={16}/>
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent>
+                        <SheetHeader>
+                            <SheetTitle>Menu</SheetTitle>
+                        </SheetHeader>
+                    </SheetContent>
+                </Sheet>
             </CardContent>
         </Card>
     );
