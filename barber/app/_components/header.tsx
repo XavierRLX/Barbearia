58b63766dc415ab80d27ed5838d3079d7c3c0344
XavierRLX <91702228+XavierRLX@users.noreply.github.com>
2024-03-22 14:07:59 -0300

    feat
    add auth provider

--- barber/app/_components/header.tsx
+++ barber/app/_components/header.tsx
@@ -3,34 +3,18 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { signIn, useSession } from "next-auth/react"
-import { Account, User } from "@prisma/client";
-
-// interface HeaderProps {
-//     user : User
-// }
+import { signIn, signOut, useSession } from "next-auth/react"
+import { MenuIcon } from "lucide-react";
 
 
-const Header = (
-    //{user} : HeaderProps
-    ) => {
-
-    const {data} = useSession();
-    const handleLoginClick = async () => {
-        await signIn();
-    };
+const Header = () => {
     return (
         <Card >
             <CardContent className="py-5 px-30 justify-between items-center flex flex-row">
                 <Image src="/logo.png" alt="FSWBarber" height={22} width={120}/>
-                {/* <Button variant={"outline"} size={"icon"} className="h-8 w-8">
-                    <MenuIcon/>
-                </Button> */}
-                {data?.user ? (
-                    <h1>{data.user.name}</h1>
-                ) : (
-                <Button onClick={handleLoginClick}>Login</Button>
-                )}
+                <Button variant={"outline"} size={"icon"} className="h-8 w-8">
+                    <MenuIcon size={16}/>
+                </Button>
             </CardContent>
         </Card>
     );
