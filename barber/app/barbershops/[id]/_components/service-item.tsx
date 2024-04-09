0c5a7df46616c7816ba92002fabc98c45a10e600
XavierRLX <91702228+XavierRLX@users.noreply.github.com>
2024-04-09 12:31:06 -0300

    feat
    add service booking initial

--- barber/app/barbershops/[id]/_components/service-item.tsx
+++ barber/app/barbershops/[id]/_components/service-item.tsx
@@ -3,7 +3,14 @@
 import { Button } from "@/app/_components/ui/button";
 import { Calendar } from "@/app/_components/ui/calendar";
 import { Card, CardContent } from "@/app/_components/ui/card";
-import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/app/_components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/app/_components/ui/sheet";
 import { Barbershop, Booking, Service } from "@prisma/client";
 import { ptBR } from "date-fns/locale";
 import { signIn, useSession } from "next-auth/react";
@@ -24,8 +31,11 @@ interface ServiceItemProps {
   isAuthenticated: boolean;
 }
 
-const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps) => {
-
+const ServiceItem = ({
+  service,
+  barbershop,
+  isAuthenticated,
+}: ServiceItemProps) => {
   const [date, setDate] = useState<Date | undefined>(undefined);
   const [hour, setHour] = useState<string | undefined>();
 
@@ -46,8 +56,8 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
 
   const timeList = useMemo(() => {
     return date ? generateDayTimeList(date) : [];
-  }, [date])
-  
+  }, [date]);
+
   return (
     <Card>
       <CardContent className="p-3 w-full">
@@ -136,39 +146,49 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
 
                   <div className="py-6 px-5 border-t border-solid border-secondary">
                     <Card>
-                      <CardContent className="p-3">
+                      <CardContent className="p-3 gap-3 flex-col">
                         <div className="flex justify-between">
                           <h2 className="font-bold">{service.name}</h2>
                           <h3 className="font-bold text-sm">
                             {""}
-                            {Intl.NumberFormat("pt_BR", {
+                            {Intl.NumberFormat("pt-BR", {
                               style: "currency",
                               currency: "BRL",
-                            }).format(Number(service.price))
-                            }
-                            </h3>
+                            }).format(Number(service.price))}
+                          </h3>
+                        </div>
+
+                        {date && (
+                          <div className="flex justify-between">
+                            <h3 className="text-gray-400 text-sm">Date</h3>
+                            <h4 className="text-sm">
+                              {format(date, "dd 'de' MMMM", {
+                                locale: ptBR,
+                              })}
+                            </h4>
+                          </div>
+                        )}
+
+                        {hour && (
+                          <div className="flex justify-between">
+                            <h3 className="text-gray-400 text-sm">Horário</h3>
+                            <h4 className="text-sm">{hour}</h4>
+                          </div>
+                        )}
+
+                        <div className="flex justify-between">
+                          <h3 className="text-gray-400 text-sm">Barbearia</h3>
+                          <h4 className="text-sm">{barbershop.name}</h4>
                         </div>
                       </CardContent>
                     </Card>
                   </div>
-
-                  {date && (
-                    <div className="flex justify-between">
-                      <h3 className="text-gray-400 text-sm">Date</h3>
-                      <h4 className="text-sm">
-                        {format(date,"dd 'de' MMMM" , {
-                        locale: ptBR,
-                        })}
-                      </h4>
-                    </div>
-                  )}
-
-                  {date && (
-                    <div className="flex justify-between">
-                      <h3 className="text-gray-400 text-sm"></h3>
-                      <h4 className="text-sm">{barbershop.name}</h4>
-                    </div>
-                  )}
+                  <SheetFooter className="px-5">
+                    <Button disabled={!hour || !date}>
+                     
+                      Confirmar reserva
+                    </Button>
+                  </SheetFooter>
                 </SheetContent>
               </Sheet>
             </div>
